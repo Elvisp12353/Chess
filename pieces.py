c26050af3c6daaa883bcef7f672503fed53b718b
Elvis Genao <elvisp12353@gmail.com>
2020-06-01 12:38:04 -0400

    I'm starting the repository

---
+++ pieces.py
@@ -0,0 +1,13 @@
+#!/ust/bin/python3
+__author__ ="ELvis Genao"
+__copyright__ ="Copyright"
+__credits__ ="Elvis"
+__license__ ="GPl"
+__version__ ="1.0.1"
+__maintainer__ ="Elvis Genao"
+__email__ ="elvisp12353@gmail.com"
+__status__ ="Developer"
+
+"""
+This module make the pieces
+"""
