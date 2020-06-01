2fbdd5481258cd673b3db01c08f8f04018573bf2
Elvis Genao <elvisp12353@gmail.com>
2020-06-01 13:25:45 -0400

    Empeze a trabajar con las fichas y sus iconos en la rama de pruebas

--- pieces.py
+++ pieces.py
@@ -9,5 +9,40 @@ __email__ ="elvisp12353@gmail.com"
 __status__ ="Developer"
 
 """
-This module make the pieces
+This module make the pieces That will be used in the game
 """
+class piece(object):
+    def __init__(self,icon):
+        self.icon = icon
+        
+
+class king(piece):
+    def __init__(self):
+        self.icon = "♚"
+
+    
+class queen(piece):
+    def __init__(self):
+        self.icon = "♛"
+  
+
+class rook(piece):
+    def __init__(self):
+        self.icon = "♜"
+        
+
+class bishops(piece):
+    def __init__(self):
+        self.icon = "♝"
+
+class knights(piece):
+    def __init__(self):
+        self.icon = "♞"
+
+class pawn(piece):
+    def __init__(self):
+        self.icon = "♟"
+
+
+numbre1 = pawn()
+print(numbre1.icon)
